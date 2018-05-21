95fb87df7687c499233f310d8dafacd08b6de7a4
Harry Singh <ubikenobi@users.noreply.github.com>
2018-05-21 09:44:43 -0700

    Update main.go

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"testproject/util"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	util.GetData()
-	log.Infof("Data received...yay!")
+	logrus.Infof("Data received...yay!")
 }
